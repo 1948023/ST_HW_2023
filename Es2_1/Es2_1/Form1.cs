c722e08ae47d2dedbc5b9c830b91c2a0bc39db28
1948023 <126237422+1948023@users.noreply.github.com>
2023-10-08 11:31:39 +0200

    Add files via upload

---
+++ Es2_1/Es2_1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen blkpen = new Pen(Color.FromArgb(255, 0, 0, 0), 5);
+
+            //linea
+            Point point1 = new Point(10, 10);
+            Point point2 = Point.Add(point1, new Size(255, 0));
+            e.Graphics.DrawLine(blkpen, point1, point2);
+
+            //punto
+            Point point3 = new Point(300, 10);
+            Point point4 = Point.Add(point3, new Size(1, 0));
+            e.Graphics.DrawLine(blkpen, point3, point4);
+
+            //cerchio
+            e.Graphics.DrawEllipse(blkpen, 350, 10, 30, 30);
+            
+
+            //rettangolo
+            e.Graphics.DrawRectangle(blkpen, 400, 10, 150, 90);
+
+            
+        }
+    }
+}
