1fe92b1f101b3101109ea12693e593767eb89ecd
1948023 <126237422+1948023@users.noreply.github.com>
2023-10-14 12:35:29 +0200

    Add files via upload

---
+++ HW2/Es3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Define the number of random variates (N) and the number of class intervals (k).
+        int N = 1000;
+        int k = 10;
+
+        // Initialize a random number generator.
+        Random random = new Random();
+
+        // Initialize an array to store the counts for each class interval.
+        int[] counts = new int[k];
+
+        // Generate random variates and count them in class intervals.
+        for (int i = 0; i < N; i++)
+        {
+            double randomValue = random.NextDouble(); // Generate a random value in [0,1).
+            int interval = (int)(randomValue * k);    // Determine the interval index.
+            counts[interval]++;                        // Increment the count for the interval.
+        }
+
+        // Display the counts for each class interval.
+        Console.WriteLine("Class Intervals and Counts:");
+        for (int i = 0; i < k; i++)
+        {
+            double lowerBound = (double)i / k;
+            double upperBound = (double)(i + 1) / k;
+            Console.WriteLine($"[{lowerBound:f2}, {upperBound:f2}): {counts[i]}");
+        }
+    }
+}
