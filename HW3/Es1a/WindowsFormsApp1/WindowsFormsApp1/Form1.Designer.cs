273c889368d928f2415b375bc8d2c13031060bd1
1948023 <126237422+1948023@users.noreply.github.com>
2023-10-22 11:45:59 +0200

    Add files via upload

---
+++ HW3/Es1a/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.NTextBox = new System.Windows.Forms.TextBox();
+            this.pTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Es1 a";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MTextBox
+            // 
+            this.MTextBox.Location = new System.Drawing.Point(12, 28);
+            this.MTextBox.Name = "MTextBox";
+            this.MTextBox.Size = new System.Drawing.Size(100, 22);
+            this.MTextBox.TabIndex = 1;
+            this.MTextBox.Text = "8";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SimulateButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(48, 115);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(687, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // NTextBox
+            // 
+            this.NTextBox.Location = new System.Drawing.Point(138, 28);
+            this.NTextBox.Name = "NTextBox";
+            this.NTextBox.Size = new System.Drawing.Size(100, 22);
+            this.NTextBox.TabIndex = 4;
+            this.NTextBox.Text = "8";
+            // 
+            // pTextBox
+            // 
+            this.pTextBox.Location = new System.Drawing.Point(268, 28);
+            this.pTextBox.Name = "pTextBox";
+            this.pTextBox.Size = new System.Drawing.Size(100, 22);
+            this.pTextBox.TabIndex = 5;
+            this.pTextBox.Text = "0.5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pTextBox);
+            this.Controls.Add(this.NTextBox);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox NTextBox;
+        private System.Windows.Forms.TextBox pTextBox;
+    }
+}
+
---
+++ HW3/Es1a/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void SimulateButton_Click(object sender, EventArgs e)
+        {
+            // Read input values
+            int M = int.Parse(MTextBox.Text);
+            int N = int.Parse(NTextBox.Text);
+            double p = double.Parse(pTextBox.Text);
+
+            // Clear the chart
+            chart1.Series.Clear();
+
+            // Simulate the score trajectories for M systems
+            for (int systemIndex = 1; systemIndex <= M; systemIndex++)
+            {
+                Series series = new Series("System " + systemIndex);
+                series.ChartType = SeriesChartType.Line;
+
+                double score = 1.0;
+
+                for (int attack = 1; attack <= N; attack++)
+                {
+                    // Simulate penetration with probability p
+                    if (RandomNumberGenerator.NextDouble() < p)
+                    {
+                        score = -1.0;
+                    }
+
+                    series.Points.AddXY(attack, score);
+                }
+
+                chart1.Series.Add(series);
+            }
+        }
+
+        private Random RandomNumberGenerator = new Random();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
