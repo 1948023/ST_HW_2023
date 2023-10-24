ef9b2e02417e02e427622cf7212ba7fa0359f627
1948023 <126237422+1948023@users.noreply.github.com>
2023-10-24 18:07:31 +0200

    Add files via upload

---
+++ Tesi/LLN/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SimulateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(12, 20);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(41, 16);
+            this.ResultLabel.TabIndex = 0;
+            this.ResultLabel.Text = "Label";
+            this.ResultLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // SimulateButton
+            // 
+            this.SimulateButton.Location = new System.Drawing.Point(15, 49);
+            this.SimulateButton.Name = "SimulateButton";
+            this.SimulateButton.Size = new System.Drawing.Size(75, 23);
+            this.SimulateButton.TabIndex = 1;
+            this.SimulateButton.Text = "Button";
+            this.SimulateButton.UseVisualStyleBackColor = true;
+            this.SimulateButton.Click += new System.EventHandler(this.SimulateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SimulateButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button SimulateButton;
+    }
+}
+
---
+++ Tesi/LLN/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SimulateButton_Click(object sender, EventArgs e)
+        {
+            // Numero di lanci del dado in ogni simulazione
+            int numRolls = 100;
+
+            // Numero di simulazioni
+            int numSimulations = 1000;
+
+            // Array per memorizzare le medie di ciascuna simulazione
+            double[] means = new double[numSimulations];
+
+            // Simulazione
+            for (int i = 0; i < numSimulations; i++)
+            {
+                int[] rolls = new int[numRolls];
+
+                // Simula il lancio del dado
+                for (int j = 0; j < numRolls; j++)
+                {
+                    rolls[j] = random.Next(1, 7); // Un dado a 6 facce
+                }
+
+                // Calcola la media dei lanci e la salva nell'array delle medie
+                means[i] = CalculateMean(rolls);
+            }
+
+            // Calcola la media delle medie
+            double averageMean = CalculateMeand(means);
+
+            // Visualizza il risultato nel label
+            ResultLabel.Text = $"Media delle medie dopo {numRolls} lanci in {numSimulations} simulazioni: {averageMean:F4}";
+        }
+
+        // Calcola la media di un array di valori
+        private double CalculateMean(int[] values)
+        {
+            double sum = 0;
+            foreach (var value in values)
+            {
+                sum += value;
+            }
+            return sum / values.Length;
+        }
+
+        private double CalculateMeand(double[] values)
+        {
+            double sum = 0;
+            foreach (var value in values)
+            {
+                sum += value;
+            }
+            return sum / values.Length;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
