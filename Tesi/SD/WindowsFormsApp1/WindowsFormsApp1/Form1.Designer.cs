c8cc3e7d43fa84f0229febceea11ef81e61853d8
1948023 <126237422+1948023@users.noreply.github.com>
2023-11-13 18:01:11 +0100

    Add files via upload

---
+++ Tesi/SD/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.Button = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.CheckBox();
+            this.radioButton2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(247, 68);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(44, 16);
+            this.resultLabel.TabIndex = 0;
+            this.resultLabel.Text = "label1";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 65);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(16, 104);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(75, 23);
+            this.Button.TabIndex = 3;
+            this.Button.Text = "Normal Button";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(170, 133);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 20);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.Text = "Normal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(304, 130);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 20);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Uniform";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.CheckBox radioButton1;
+        private System.Windows.Forms.CheckBox radioButton2;
+    }
+}
+
---
+++ Tesi/SD/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double userInput = Convert.ToDouble(inputTextBox.Text);
+
+            if (radioButton1.Checked) // Normal Distribution
+            {
+                double result = calculateNormalDistribution(userInput);
+                displayResult(result);
+            }
+            else if (radioButton2.Checked) // Uniform Distribution
+            {
+                double result = calculateUniformDistribution(userInput);
+                displayResult(result);
+            }
+        }
+
+        private double calculateNormalDistribution(double x)
+        {
+            double mean = 50;
+            double standardDeviation = 10;
+            double constant = 1 / (standardDeviation * Math.Sqrt(2 * Math.PI));
+            double power = -1 * Math.Pow((x - mean) / standardDeviation, 2) / 2;
+            double result = constant * Math.Exp(power);
+            return result;
+        }
+
+        private double calculateUniformDistribution(double x)
+        {
+            double min = 0;
+            double max = 100;
+            double result = x >= min && x <= max ? 1 / (max - min) : 0;
+            return result;
+        }
+
+        private void displayResult(double result)
+        {
+            resultLabel.Text = $"Result: {result.ToString("F2")}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
