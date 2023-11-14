2b8dd449f934b26b0f57fd671b3eb1b9324fc9cd
1948023 <126237422+1948023@users.noreply.github.com>
2023-11-14 18:24:47 +0100

    Add files via upload

---
+++ Tesi/ITO/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button = new System.Windows.Forms.Button();
+            this.integralResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(45, 73);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(75, 23);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Button";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // integralResultLabel
+            // 
+            this.integralResultLabel.AutoSize = true;
+            this.integralResultLabel.Location = new System.Drawing.Point(150, 76);
+            this.integralResultLabel.Name = "integralResultLabel";
+            this.integralResultLabel.Size = new System.Drawing.Size(44, 16);
+            this.integralResultLabel.TabIndex = 1;
+            this.integralResultLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.integralResultLabel);
+            this.Controls.Add(this.Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Label integralResultLabel;
+    }
+}
+
---
+++ Tesi/ITO/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Random random = new Random();
+        private const int numSteps = 100;
+        private double integralResult = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SimulateItoIntegration()
+        {
+            for (int i = 0; i < numSteps; i++)
+            {
+                double integrand = random.NextDouble(); // Replace with your specific integrand
+                double dW = random.NextGaussian(); // Wiener increment
+
+                integralResult += integrand * dW;
+
+                // Update UI or perform other actions for each step
+                UpdateIntegralResultLabel();
+
+                // Optional: Add a delay to visualize the simulation
+                Thread.Sleep(50);
+            }
+        }
+
+        private void UpdateIntegralResultLabel()
+        {
+            if (integralResultLabel.InvokeRequired)
+            {
+                integralResultLabel.Invoke(new Action(UpdateIntegralResultLabel));
+            }
+            else
+            {
+                integralResultLabel.Text = $"Integral Result: {integralResult:F2}";
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Start the Ito Integration simulation in a separate thread
+            Thread simulationThread = new Thread(SimulateItoIntegration);
+            simulationThread.Start();
+        }
+    }
+
+    // Extension method to generate random Gaussian values
+    public static class RandomExtensions
+    {
+        public static double NextGaussian(this Random random)
+        {
+            double u1 = 1.0 - random.NextDouble();
+            double u2 = 1.0 - random.NextDouble();
+            return Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+        }
+    }
+}
