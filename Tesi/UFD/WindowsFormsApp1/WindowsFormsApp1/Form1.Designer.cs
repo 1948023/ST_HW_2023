4a2df7d2ece355397bb92e3c26f069155b97da1e
1948023 <126237422+1948023@users.noreply.github.com>
2023-11-14 17:54:40 +0100

    Add files via upload

---
+++ Tesi/UFD/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.assetPriceLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // assetPriceLabel
+            // 
+            this.assetPriceLabel.AutoSize = true;
+            this.assetPriceLabel.Location = new System.Drawing.Point(164, 56);
+            this.assetPriceLabel.Name = "assetPriceLabel";
+            this.assetPriceLabel.Size = new System.Drawing.Size(44, 16);
+            this.assetPriceLabel.TabIndex = 0;
+            this.assetPriceLabel.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.assetPriceLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label assetPriceLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tesi/UFD/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Random random = new Random();
+        private const double mu = 0.05;   // Drift rate
+        private const double sigma = 0.2; // Volatility
+        private double assetPrice = 100;  // Initial asset price
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SimulateGBM()
+        {
+            double dt = 0.01; // Time increment
+            double totalTime = 1; // Total simulation time
+            int steps = (int)(totalTime / dt);
+
+            for (int i = 0; i < steps; i++)
+            {
+                double dW = random.NextGaussian() * Math.Sqrt(dt);
+                assetPrice *= Math.Exp((mu - 0.5 * sigma * sigma) * dt + sigma * dW);
+
+                // Update UI or perform other actions for each step
+                UpdateAssetPriceLabel();
+
+                // Optional: Add a delay to visualize the simulation
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+        private void UpdateAssetPriceLabel()
+        {
+            if (assetPriceLabel.InvokeRequired)
+            {
+                assetPriceLabel.Invoke(new Action(UpdateAssetPriceLabel));
+            }
+            else
+            {
+                assetPriceLabel.Text = $"Asset Price: {assetPrice:F2}";
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Start the GBM simulation in a separate thread
+            System.Threading.Thread simulationThread = new System.Threading.Thread(SimulateGBM);
+            simulationThread.Start();
+        }
+    }
+
+    // Extension method to generate random Gaussian values
+    public static class RandomExtensions
+    {
+        public static double NextGaussian(this Random random)
+        {
+            double u1 = 1.0 - random.NextDouble();
+            double u2 = 1.0 - random.NextDouble();
+            return Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+        }
+    }
+}
+
